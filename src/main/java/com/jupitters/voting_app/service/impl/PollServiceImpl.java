170b8441dd49021ac84cd65f0d1f3af875771a95
jupitters <sampaiobale@gmail.com>
2026-03-24 16:19:42 -0300

    poll service

---
+++ src/main/java/com/jupitters/voting_app/service/impl/PollServiceImpl.java
@@ -0,0 +1,13 @@
+package com.jupitters.voting_app.service.impl;
+
+import com.jupitters.voting_app.model.Poll;
+import com.jupitters.voting_app.service.PollService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PollServiceImpl implements PollService {
+    @Override
+    public Poll createPoll(Poll poll) {
+        return null;
+    }
+}
