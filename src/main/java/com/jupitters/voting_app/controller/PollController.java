ff4c0619f8c31a1a189aa9d0ec788d1596bba796
jupitters <sampaiobale@gmail.com>
2026-03-24 16:18:02 -0300

    poll controller

--- src/main/java/com/jupitters/voting_app/controller/PollController.java
+++ src/main/java/com/jupitters/voting_app/controller/PollController.java
@@ -1,4 +1,14 @@
 package com.jupitters.voting_app.controller;
 
+import com.jupitters.voting_app.model.Poll;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/polls")
 public class PollController {
+    public Poll createPoll(@RequestBody Poll poll){
+
+    }
 }
