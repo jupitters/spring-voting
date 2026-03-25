142a948614680bbf7585104d44a36604be49c383
jupitters <sampaiobale@gmail.com>
2026-03-25 11:48:20 -0300

    option vote

--- src/main/java/com/jupitters/voting_app/model/OptionVote.java
+++ src/main/java/com/jupitters/voting_app/model/OptionVote.java
@@ -1,8 +1,10 @@
 package com.jupitters.voting_app.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class OptionVote {
     private String option;
     private Long voteCount;
