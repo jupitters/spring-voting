f532140753a9fdfae2dff33d999893892a7ccabd
jupitters <sampaiobale@gmail.com>
2026-03-26 11:54:01 -0300

    poll controller

--- src/main/java/com/jupitters/voting_app/controller/PollController.java
+++ src/main/java/com/jupitters/voting_app/controller/PollController.java
@@ -3,6 +3,7 @@ package com.jupitters.voting_app.controller;
 import com.jupitters.voting_app.model.Poll;
 import com.jupitters.voting_app.service.PollService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,5 +22,10 @@ public class PollController {
     @GetMapping
     public List<Poll> getAllPolls(){
         return pollService.getAllPolls();
+    }
+
+    public ResponseEntity<Poll> getPoll(Long id){
+        Poll poll = pollService.getPoll(id);
+        return ResponseEntity.ok(poll);
     }
 }
