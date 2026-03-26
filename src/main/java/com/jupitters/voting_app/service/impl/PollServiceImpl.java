2e5a39816a6461425936752a3c2dc9bfebca724e
jupitters <sampaiobale@gmail.com>
2026-03-26 10:53:37 -0300

    poll service

--- src/main/java/com/jupitters/voting_app/service/impl/PollServiceImpl.java
+++ src/main/java/com/jupitters/voting_app/service/impl/PollServiceImpl.java
@@ -1,13 +1,26 @@
 package com.jupitters.voting_app.service.impl;
 
 import com.jupitters.voting_app.model.Poll;
+import com.jupitters.voting_app.repository.PollRepository;
 import com.jupitters.voting_app.service.PollService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class PollServiceImpl implements PollService {
+    private final PollRepository pollRepository;
+
     @Override
     public Poll createPoll(Poll poll) {
+        return pollRepository.save(poll);
+    }
+
+    @Override
+    public List<Poll> getAllPolls(){
         return null;
     }
+
 }
