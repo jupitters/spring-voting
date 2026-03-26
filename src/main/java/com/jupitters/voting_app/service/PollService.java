7e227a0bce341902b06cc521f9e4bf557b9df726
jupitters <sampaiobale@gmail.com>
2026-03-26 10:50:00 -0300

    poll controller

--- src/main/java/com/jupitters/voting_app/service/PollService.java
+++ src/main/java/com/jupitters/voting_app/service/PollService.java
@@ -3,6 +3,9 @@ package com.jupitters.voting_app.service;
 import com.jupitters.voting_app.model.Poll;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+
 public interface PollService {
     Poll createPoll(Poll poll);
+    List<Poll> getAllPolls();
 }
